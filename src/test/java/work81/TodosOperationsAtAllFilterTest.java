c450b3149b5102cfc7b9931bc2ae5d422d14626d
Julia Iluhina <julia.v.iluhina@gmail.com>
2015-07-27 13:18:13 +0300

    27 07 2015 13:18

--- src/test/java/work81/TodosOperationsAtAllFilterTest.java
+++ src/test/java/work81/TodosOperationsAtAllFilterTest.java
@@ -89,7 +89,7 @@ public class TodosOperationsAtAllFilterTest extends TodosBaseTest {
         page.add("t1", "t2");
         page.toggle("t2");
 
-        //Switch At Commleted Filter
+        //Switch At Completed Filter
         page.filterCompleted();
         page.assertVisibleTasks("t2");
         page.assertItemsLeftCounter(1);
